5d51a7ebc8f20bbc95b78af83f5dcdacb42daff0
danieloh <56809357+danieloh01@users.noreply.github.com>
2025-04-19 18:26:45 +0000

    new commits to js

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,6 +9,10 @@ class CommentsController < ApplicationController
 
   # GET /comments/1/edit
   def edit
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   # POST /comments or /comments.json
@@ -20,11 +24,17 @@ class CommentsController < ApplicationController
       if @comment.save
         format.html { redirect_back fallback_location: root_path, notice: "Comment was successfully created." }
         format.json { render :show, status: :created, location: @comment }
+        format.js { render template: "comments/create" }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
+
+    # # respond_to do |format|
+    # #   # Handle JSON and HTML formats as usual
+    # #   format.js
+    # end
   end
 
   # PATCH/PUT /comments/1 or /comments/1.json
@@ -33,6 +43,7 @@ class CommentsController < ApplicationController
       if @comment.update(comment_params)
         format.html { redirect_to root_url, notice: "Comment was successfully updated." }
         format.json { render :show, status: :ok, location: @comment }
+        format.js
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
@@ -46,6 +57,7 @@ class CommentsController < ApplicationController
     respond_to do |format|
       format.html { redirect_back fallback_location: root_url, notice: "Comment was successfully destroyed." }
       format.json { head :no_content }
+      format.js { render template: "comments/destroy" }
     end
   end
 
